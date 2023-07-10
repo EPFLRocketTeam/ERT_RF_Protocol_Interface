c1916f951103b76eb94e02bb8d1fcee244b94b37
YohanHadji <hadji.yohan@gmail.com>
2023-07-10 12:40:24 +0200

    Add RF parameters to be used on all systems

---
+++ ParameterDefinition.h
@@ -0,0 +1,27 @@
+
+#define AV_UPLINK_FREQUENCY 867e6
+#define AV_UPLINK_POWER 20
+#define AV_UPLINK_BW 125e3
+#define AV_UPLINK_SF 8
+#define AV_UPLINK_CR 7
+#define AV_UPLINK_PREAMBLE_LEN 8
+#define AV_UPLINK_SYNC_WORD 0x12
+#define AV_UPLINK_CRC true
+
+#define AV_DOWNLINK_FREQUENCY 868e6
+#define AV_DOWNLINK_POWER 20
+#define AV_DOWNLINK_BW 125e3
+#define AV_DOWNLINK_SF 8
+#define AV_DOWNLINK_CR 7
+#define AV_DOWNLINK_PREAMBLE_LEN 8
+#define AV_DOWNLINK_SYNC_WORD 0x13
+#define AV_DOWNLINK_CRC true
+
+#define GSE_FREQUENCY 869e6
+#define GSE_POWER 20
+#define GSE_BW 125e3
+#define GSE_SF 8
+#define GSE_CR 7
+#define GSE_PREAMBLE_LEN 8
+#define GSE_SYNC_WORD 0x14
+#define GSE_CRC true
