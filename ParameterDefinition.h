10e4e28327b5bf6399f2a8c763b3502d88dbbeb4
YohanHadji <hadji.yohan@gmail.com>
2023-07-11 14:54:13 +0200

    Correct uplink and downlink references

--- ParameterDefinition.h
+++ ParameterDefinition.h
@@ -1,17 +1,17 @@
 
 // -------- AV_UPLINK -------- // 
-#define AV_UPLINK_FREQUENCY 867e6
-#define AV_UPLINK_POWER 20
-#define AV_UPLINK_BW 125e3
-#define AV_UPLINK_SF 8
-#define AV_UPLINK_CR 7
-#define AV_UPLINK_PREAMBLE_LEN 8
-#define AV_UPLINK_SYNC_WORD 0x12
-#define AV_UPLINK_CRC true
+#define UPLINK_FREQUENCY 867e6
+#define UPLINK_POWER 2
+#define UPLINK_BW 125e3
+#define UPLINK_SF 8
+#define UPLINK_CR 7
+#define UPLINK_PREAMBLE_LEN 8
+#define UPLINK_SYNC_WORD 0x12
+#define UPLINK_CRC true
 
 // -------- AV_DOWNLINK -------- // 
 #define AV_DOWNLINK_FREQUENCY 868e6
-#define AV_DOWNLINK_POWER 20
+#define AV_DOWNLINK_POWER 2
 #define AV_DOWNLINK_BW 125e3
 #define AV_DOWNLINK_SF 8
 #define AV_DOWNLINK_CR 7
@@ -20,11 +20,11 @@
 #define AV_DOWNLINK_CRC true
 
 // -------- GSE_UPLINK -------- // 
-#define GSE_FREQUENCY 869e6
-#define GSE_POWER 20
-#define GSE_BW 125e3
-#define GSE_SF 8
-#define GSE_CR 7
-#define GSE_PREAMBLE_LEN 8
-#define GSE_SYNC_WORD 0x14
-#define GSE_CRC true
+#define GSE_DOWNLINK_FREQUENCY 869e6
+#define GSE_DOWNLINK_POWER 2
+#define GSE_DOWNLINK_BW 125e3
+#define GSE_DOWNLINK_SF 8
+#define GSE_DOWNLINK_CR 7
+#define GSE_DOWNLINK_PREAMBLE_LEN 8
+#define GSE_DOWNLINK_SYNC_WORD 0x14
+#define GSE_DOWNLINK_CRC true
