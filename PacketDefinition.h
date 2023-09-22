3bc595ab05e0566cbdd58f71eff8302ab5098b2b
YohanHadji <hadji.yohan@gmail.com>
2023-09-22 13:42:26 +0200

    Updating tracker packet. Not touching AV

--- PacketDefinition.h
+++ PacketDefinition.h
@@ -243,6 +243,9 @@ typedef struct __attribute__((__packed__)) {
 	float azm;
 	float elv;
 	int mode;
+	float cutoffFreq;
+	unsigned maxTimeWindow;
+	unsigned timeStamp;
 } PacketTrackerCmd;
 #ifdef __cplusplus
 const uint32_t packetTrackerCmdSize = sizeof(PacketTrackerCmd);
