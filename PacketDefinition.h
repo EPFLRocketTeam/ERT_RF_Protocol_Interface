7fbca9cbc4ae16b9cac31924b26fadc80c86d677
Maxime Rochat <rochat.max@outlook.com>
2025-02-15 17:01:22 +0100

    added an icarus packet

--- PacketDefinition.h
+++ PacketDefinition.h
@@ -52,6 +52,7 @@ enum CAPSULE_ID {
 	// Rocket & GSE
 	AV_TELEMETRY = 8,
 	GSE_TELEMETRY,
+	HOPPER_DOWNLINK,
 	GS_CMD, // uplink from GS
 	//////////////////////////////////
 	// Tracker
@@ -171,6 +172,46 @@ typedef struct __attribute__((__packed__)) {
 const uint32_t av_downlink_size = sizeof(av_downlink_t);
 #endif
 
+
+// ---------------------- HOPPER PACKET ---------------------- // 
+typedef struct __attribute__((__packed__)) {
+    uint32_t packet_nbr;      // 32 bits: packet counter (10Hz update rate)
+    uint16_t N2O_pressure;    // 16 bits (only 12 bits used from ADC)
+    uint16_t ETH_pressure;    // 16 bits
+    uint8_t  N2O_temp;        // 8 bits
+    struct {
+        unsigned int N2O_vent : 1;  // 1 bit
+        unsigned int ETH_vent : 1;  // 1 bit
+    } vents;                    // Together: 2 bits (packed with next field)
+    uint8_t  N2O_main;        // 8 bits
+    uint8_t  ETH_main;        // 8 bits
+    float    gnss_lon;        // 32 bits (IEEE 754)
+    float    gnss_lat;        // 32 bits (IEEE 754)
+    uint8_t  sat_nbr;         // 8 bits: number of fixed satellites
+    int16_t  gyro_x;          // 16 bits: raw gyro values
+    int16_t  gyro_y;          // 16 bits
+    int16_t  gyro_z;          // 16 bits
+    int16_t  acc_x;           // 16 bits: raw accelerometer values
+    int16_t  acc_y;           // 16 bits
+    int16_t  acc_z;           // 16 bits
+    int16_t  baro;            // 16 bits: barometric reading
+    int16_t  kalman_pos_x;    // 16 bits: Kalman filter estimated positions
+    int16_t  kalman_pos_y;    // 16 bits
+    int16_t  kalman_pos_z;    // 16 bits
+    int16_t  kalman_yaw;      // 16 bits: Kalman filter estimated angles
+    int16_t  kalman_pitch;    // 16 bits
+    int16_t  kalman_roll;     // 16 bits
+    uint8_t  gimbal_x;        // 8 bits: gimbal position
+    uint8_t  gimbal_y;        // 8 bits
+    uint8_t  HV_voltage;      // 8 bits: high-voltage measurement
+    uint8_t  LV_voltage;      // 8 bits: low-voltage measurement
+    uint8_t  AV_temp;         // 8 bits: AV temperature
+    // Total: 410 bits (51.25 bytes)
+} PacketHopper_downlink;
+
+#ifdef __cplusplus
+const uint32_t packetHopper_downlink_size = sizeof(PacketHopper_downlink);
+#endif
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 ////////// FROM HERE, GS stuff, please don't touch //////////////
@@ -264,47 +305,46 @@ const uint32_t packetTrackerCmdSize = sizeof(PacketTrackerCmd);
 
 enum GUI_CMD {
 	GET_SERIAL_STATUS = 0,
-
 };
 
 
 enum GUI_FIELD {
-    IGNITER_LOX,
-    IGNITER_FUEL,
-    MAIN_LOX,
-    MAIN_FUEL,
-    VENT_LOX,
-    VENT_FUEL,
-    ORDER_ID,
-    ORDER_VALUE,
-    PACKET_NBR,
-    TIMESTAMP,
-    GNSS_LON,
-    GNSS_LAT,
-    GNSS_ALT,
-    GNSS_LON_R,
-    GNSS_LAT_R,
-    GNSS_ALT_R,
-    GNSS_VERTICAL_SPEED,
-    N2_PRESSURE,
-    FUEL_PRESSURE,
-    LOX_PRESSURE,
-    FUEL_LEVEL,
-    LOX_LEVEL,
-    ENGINE_TEMP,
-    IGNITER_PRESSURE,
-    LOX_INJ_PRESSURE,
-    FUEL_INJ_PRESSURE,
-    CHAMBER_PRESSURE,
-    AV_STATE,
-    GNSS_CHOICE,
-    FILLINGN2O,
-    VENT_GSE,
-    TANK_PRESSURE,
-    TANK_TEMPERATURE,
-    FILLING_PRESSURE,
-    DISCONNECT_ACTIVE,
-    LOADCELL_RAW,
+	IGNITER_LOX,
+	IGNITER_FUEL,
+	MAIN_LOX,
+	MAIN_FUEL,
+	VENT_LOX,
+	VENT_FUEL,
+	ORDER_ID,
+	ORDER_VALUE,
+	PACKET_NBR,
+	TIMESTAMP,
+	GNSS_LON,
+	GNSS_LAT,
+	GNSS_ALT,
+	GNSS_LON_R,
+	GNSS_LAT_R,
+	GNSS_ALT_R,
+	GNSS_VERTICAL_SPEED,
+	N2_PRESSURE,
+	FUEL_PRESSURE,
+	LOX_PRESSURE,
+	FUEL_LEVEL,
+	LOX_LEVEL,
+	ENGINE_TEMP,
+	IGNITER_PRESSURE,
+	LOX_INJ_PRESSURE,
+	FUEL_INJ_PRESSURE,
+	CHAMBER_PRESSURE,
+	AV_STATE,
+	GNSS_CHOICE,
+	FILLINGN2O,
+	VENT_GSE,
+	TANK_PRESSURE,
+	TANK_TEMPERATURE,
+	FILLING_PRESSURE,
+	DISCONNECT_ACTIVE,
+	LOADCELL_RAW,
 	ENGINE_STATE,
 	GSE_VENT,
 	GSE_CMD_STATUS,
@@ -333,6 +373,38 @@ enum GUI_FIELD {
 	GUI_CMD_FILLING_LOX,
 	GUI_CMD_VENT,
 	GUI_CMD_DISCONNECT,
+	HOPPER_PACKET_NBR,      // 32 bits: packet counter (10Hz update)
+	HOPPER_N2O_PRESSURE,    // 16 bits (12-bit ADC value)
+	HOPPER_ETH_PRESSURE,    // 16 bits
+	HOPPER_N2O_TEMP,        // 8 bits
+	HOPPER_N2O_VENT,        // 1 bit
+	HOPPER_ETH_VENT,        // 1 bit
+	HOPPER_N2O_MAIN,        // 8 bits
+	HOPPER_ETH_MAIN,        // 8 bits
+	HOPPER_GNSS_LON,        // 32 bits (float)
+	HOPPER_GNSS_LAT,        // 32 bits (float)
+	HOPPER_SAT_NBR,         // 8 bits (number of fixed satellites)
+	HOPPER_GYRO_X,          // 16 bits (raw gyro)
+	HOPPER_GYRO_Y,          // 16 bits
+	HOPPER_GYRO_Z,          // 16 bits
+	HOPPER_ACC_X,           // 16 bits (raw accelerometer)
+	HOPPER_ACC_Y,           // 16 bits
+	HOPPER_ACC_Z,           // 16 bits
+	HOPPER_BARO,            // 16 bits (barometric reading)
+	HOPPER_KALMAN_POS_X,    // 16 bits (Kalman filter position estimate)
+	HOPPER_KALMAN_POS_Y,    // 16 bits
+	HOPPER_KALMAN_POS_Z,    // 16 bits
+	HOPPER_KALMAN_YAW,      // 16 bits (Kalman filter angle)
+	HOPPER_KALMAN_PITCH,    // 16 bits
+	HOPPER_KALMAN_ROLL,     // 16 bits
+	HOPPER_GIMBAL_X,        // 8 bits (gimbal position)
+	HOPPER_GIMBAL_Y,        // 8 bits
+	HOPPER_HV_VOLTAGE,      // 8 bits (high-voltage measurement)
+	HOPPER_LV_VOLTAGE,      // 8 bits (low-voltage measurement)
+	HOPPER_AV_TEMP,         // 8 bits (AV temperature)
+	HOPPER_ID_CONFIG,       // 8 bits (configuration ID: PID, flight mode, etc.)
+	HOPPER_AV_STATE,        // 8 bits (AV state)
+	HOPPER_FIELD_COUNT,      // Total number of Hopper packet fields
 	UNKNOWN
 };
 
