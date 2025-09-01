2606387a0a113bd30860f827ca0fc46f58f52209
Lionel-NUC <johndoe@example.com>
2025-09-01 10:36:11 +0200

    yet again...

---
+++ build/tests/CMakeFiles/downlink_compression_test.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ert/2024_C_GS_SOFTWARE/ERT_RF_Protocol_Interface/tests/downlink_compression_test.cc" "tests/CMakeFiles/downlink_compression_test.dir/downlink_compression_test.cc.o" "gcc" "tests/CMakeFiles/downlink_compression_test.dir/downlink_compression_test.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/tests/CMakeFiles/downlink_compression_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/downlink_compression_test.dir/downlink_compression_test.cc.o"
+  "CMakeFiles/downlink_compression_test.dir/downlink_compression_test.cc.o.d"
+  "downlink_compression_test"
+  "downlink_compression_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/downlink_compression_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
